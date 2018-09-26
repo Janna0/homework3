27cdbc389c75361e2d10780cd54abb9b81941942
Janna0 <43066258+Janna0@users.noreply.github.com>
2018-09-26 14:21:56 +0400

    Update homework3.js

--- homework3/homework3.js
+++ homework3/homework3.js
@@ -4,7 +4,7 @@ return (a===b+c || b===a*c);
 
 const f2 = function (){
 	return ('Hello');
-}
+};
 
 
 
@@ -16,7 +16,7 @@ const  z = (x*y)/100;
 
 
 
-}
+};
 
 
 const f4 = function (name , name1 , name2){
@@ -36,7 +36,7 @@ else{
 	return "some names have the same length";
 }
 
-}
+};
 
 
 
@@ -51,16 +51,16 @@ const f5 = function (a,b){
      else{
      	return -1;
      }
-}
+};
 
 
 const f6 = function (a,b){
 	return a*b;
-}
+};
 
 const f7 = function (a,b){
 	return a/b;
-}
+};
 
 
 const triangleArea = function (base, height){
@@ -68,7 +68,7 @@ const triangleArea = function (base, height){
 return f7(f6(base, height),2);
 
 
-}
+};
 
 
 
@@ -77,7 +77,7 @@ const numLenght = function(a){
 	const num = a+"";
 	return num.length;
 }
-
+;
 
 
 const f8 = function (a,b,c){
@@ -87,7 +87,7 @@ const f8 = function (a,b,c){
 else{
 	return -1;
 }
-}
+};
 
 
 const runStuff = function (a,b,c){
@@ -100,5 +100,5 @@ const runStuff = function (a,b,c){
 	else{
 		return -1;
 	}
-}
+};
 
